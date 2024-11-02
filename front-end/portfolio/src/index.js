271d4ad88b3d6d363aaee70ebd25bec5cf6c5345
Sehara2002 <fernandomsa.22@uom.lk>
2024-11-03 00:04:53 +0530

    Adding Pages

--- front-end/portfolio/src/index.js
+++ front-end/portfolio/src/index.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import {createBrowserRouter, Router, RouterProvider} from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Projects from './Pages/Projects/Projects';
+import Services from './Pages/Services/Services';
+import Contact from './Pages/Contact/Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const router = createBrowserRouter([
   {
     path:"/",
-    element:<App/>,
+    element:<Home/>,
     errorElement:"404 Not Found",
   },
+  {
+    path:"/about",
+    element:<About/>,
+    errorElement:"404 Not Found",
+  },
+  {
+    path:"/projects",
+    element:<Projects/>,
+    errorElement:"404 Not Found",
+  },
+  {
+    path:"/services",
+    element:<Services/>,
+    errorElement:"404 Not Found",
+  },
+  {
+    path:"/contact",
+    element:<Contact/>,
+    errorElement:"404 Not Found",
+  }
+
 ])  
 root.render(
   <React.StrictMode>
